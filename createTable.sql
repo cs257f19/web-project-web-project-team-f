562258439446b2b48b178a235348140e91751a5f
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-10-16 12:15:40 -0500

    Add files via upload

---
+++ createTable.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS Serving;
+CREATE TABLE Serving (
+  NDB_No integer,
+  Serving_Size integer,
+  Serving_Size_UOM text,
+  Household_Serving_Size integer,
+  Household_Serving_Size_UOM text,
+);
