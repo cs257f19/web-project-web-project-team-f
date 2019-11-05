9aa6f943098b880c55358a69ef3fe95ae203bf2e
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-11-04 22:54:55 -0600

    Add files via upload

--- createtable.sql
+++ createtable.sql
@@ -1,12 +1,55 @@
-DROP TABLE IF EXISTS earthquakes;
-CREATE TABLE earthquakes (
-  quakedate date,
-  quaketime time,
-  latitude real,
-  longitude real,
-  quakedepth real,
-  mag real,
-  id text,
-  place text,
-  quaketype text
+DROP TABLE IF EXISTS Nutrek;
+CREATE TABLE Nutrek(
+  long_name text;
+  manufacturer text;
+  ingredients_english text;
+  Serving_Size real;
+  Serving_Size_UOM text;
+  Household_Serving_Size real;
+  Household_Serving_Size_UOM text;
+  Ash (g) real;
+  Biotin (mcg) real;
+  Caffeine (mg) real;
+  Calcium, Ca (mg) real;
+  Carbohydrate, by difference (g) real;
+  Carbohydrate, other (g) real;
+  Cholesterol (mg) real;
+  Chromium, Cr (mcg) real;
+  Copper, Cu (mg) real;
+  Fatty acids, total monounsaturated (g) real;
+  Fatty acids, total polyunsaturated (g) real;
+  Fatty acids, total saturated (g) real;
+  Fatty acids, total trans (g) real;
+  Fiber, insoluble (g) real;
+  Fiber, soluble (g) real;
+  Fiber, total dietary (g) real;
+  Folic acid (mcg) real;
+  Iodine, I (mcg) real;
+  Iron, Fe (mg) real;
+  Lactose (g) real;
+  Magnesium, Mg (mg) real;
+  Manganese, Mn (mg) real;
+  Niacin (mg) real;
+  Pantothenic acid (mg) real;
+  Phosphorus, P (mg) real;
+  Potassium, K (mg) real;
+  Protein (g) real;
+  Riboflavin (mg) real;
+  Selenium, Se (mcg) real;
+  Sodium, Na (mg) real;
+  Sugars, added (g) real;
+  Sugars, total (g) real;
+  Thiamin (mg) real;
+  Total lipid (fat) (g) real;
+  Total sugar alcohols (g) real;
+  Vitamin A, IU (IU) real;
+  Vitamin B-12 (mcg) real;
+  Vitamin B-6 (mg) real;
+  Vitamin C, total ascorbic acid (mg) real;
+  Vitamin D (IU) real;
+  Vitamin E (label entry primarily) (IU) real;
+  Vitamin K (phylloquinone) (mcg) real;
+  Water (g) real;
+  Xylitol (g) real;
+  Zinc, Zn (mg) real;
 );
