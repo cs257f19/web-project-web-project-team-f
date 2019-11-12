53c001fa401a9b9e10e1ed4126ba925ebbaac4ba
aisheem <49172107+aisheem@users.noreply.github.com>
2019-11-12 17:22:15 -0600

    Add files via upload

--- datasource.py
+++ datasource.py
@@ -48,7 +48,7 @@ class Nutrek:
         food = str(food.upper())
         try:
             cursor = self.connection.cursor()
-            cursor.execute("SELECT * FROM Nutrek WHERE (%s) LIKE food_name",(food,))
+            cursor.execute("SELECT * FROM Nutrek WHERE food_name ~ (%s)",(food,))
             results = cursor.fetchall()
             return results
 
