2668c1448b1349a8061bc393d87ff8a3ab7c9805
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-11-15 00:14:25 -0600

    Add files via upload

--- datasource.py
+++ datasource.py
@@ -5,6 +5,8 @@ class Nutrek:
     '''
     Nutrek executes all of the queries on the database
     and formats the data to send back to the front end'''
+
+
     def connect(self, user, password):
         '''
         Establishes a connection to the database with the following credentials:
@@ -23,34 +25,37 @@ class Nutrek:
         Breaks the connection to the database
         '''
         self.connection.close()
-    
-    def getAllColumns(self):
-        '''
-        returns all columns - WILL DELETE (foundation of PSQL queries)
-        '''
-        try:
-            cursor = self.connection.cursor ()
-            cursor.execute("SELECT * FROM Nutrek")
-            results = cursor.fetchall()
-            return results
-
-        except Exception as e:
-            print ("Something went wrong when executing the query: ", e)
-            return None
 
     def getNutrients(self, food):
         '''
         returns all nutrients and the amount of each nutrient in a given food
         '''
-        # ***QUESTION 2 - Syntax okay? i.e.  <> operator, we don't need dictionary anymore? We were initially thinking of
-        # doing nutrient, nutrient amount (key, value pair in dict, and if all values added was 0, there was insufficient information in database
-        # on the nutritional breakdown of food). How can we deal with this issue?
-        food = str(food.upper())
+        food = food.upper()
+        nutrientList = ["ash(g)", "biotin(mcg)", "caffeine(mg)", "calcium(mg)", "carbohydrate by difference(g)", "carbohydrate_other(g)", "cholesterol(mg)",
+        "chromium(mcg)", "copper(mg)", "fatty acids total monounsaturated(g)", "fatty acids total polyunsaturated (g)", "fatty acids total saturated(g)", "fatty acids total trans(g)",
+        "fiber insoluble(g)", "fiber soluble(g)", "fiber total dietary(g)", "folic acid(mcg)", "iodine(mcg)", "iron(mg)", "lactose(g)",
+         "magnesium(mg)", "manganese(mg)", "niacin(mg)", "pantothenic acid(mg)", "phosphorus (mg)", "potassium(mg)",
+         "protein(g)", "riboflavin(mg)", "selenium(mcg)", "sodium(mg)", "sugars added(g)", "sugars total(g)", "thiamin(mg)", "total lipid fat(g)",
+         "total sugar alcohols(g)", "vitamin a IU" , "vitamin b 12(mcg)", "vitamin b-6(mg)", "vitamin c total ascorbic acid(mg)",
+         "vitamin d IU", "vitamin e label entry primarily IU", "vitamin K phylloquinone(mcg)", "water(g)",
+         "xylitol(g)", "zinc(mg)"]
         try:
-            cursor = self.connection.cursor ()
-            cursor.execute("SELECT food_name,Ash_grams,Biotin_mcg,Caffeine_mg,Calcium_Ca_mg,Carbohydrate_by_difference_g,Carbohydrate_other_g,Cholesterol_mg,Chromium_Cr_mcg,Copper_Cu_mg,Fatty_acids_total_monounsaturated_g,Fatty_acids_total_polyunsaturated_g,Fatty_acids_total_saturated_g,Fatty_acids_total_trans_g,Fiber_insoluble_g,Fiber_soluble_g,Fiber_total_dietary_g,Folic_acid_mcg,Iodine_I_mcg,Iron_Fe_mg,Lactose_g,Magnesium_Mg_mg,Manganese_Mn_mg,Niacin_mg,Pantothenic_acid_mg,Phosphorus_P_mg,Potassium_K_mg,Protein_g,Riboflavin_mg,Selenium_Se_mcg,Sodium_Na_mg,Sugars_added_g,Sugars_total_g,Thiamin_mg,Total_lipid_fat_g,Total_sugar_alcohols_g,Vitamin_A_IU,Vitamin_B12_mcg,Vitamin_B6_mg,Vitamin_C_total_ascorbic_acid_mg,Vitamin_D_IU,Vitamin_E_label_entry_primarily_IU,Vitamin_K_phylloquinone_mcg,Water_g,Xylitol_g,Zinc_Zn_mg FROM Nutrek WHERE (food_name ~ (%s)) AND ((Ash_grams <> 0) OR (Biotin_mcg <> 0))",(food,))
-            results = cursor.fetchall() # tuple
-            return results
+            cursor1 = self.connection.cursor()
+            cursor1.execute("SELECT Ash_grams, Biotin_mcg, Caffeine_mg, Calcium_Ca_mg, Carbohydrate_by_difference_g, Carbohydrate_other_g, Cholesterol_mg, Chromium_Cr_mcg, Copper_Cu_mg, Fatty_acids_total_monounsaturated_g, Fatty_acids_total_polyunsaturated_g, Fatty_acids_total_saturated_g, Fatty_acids_total_trans_g, Fiber_insoluble_g, Fiber_soluble_g, Fiber_total_dietary_g, Folic_acid_mcg, Iodine_I_mcg, Iron_Fe_mg, Lactose_g, Magnesium_Mg_mg, Manganese_Mn_mg, Niacin_mg, Pantothenic_acid_mg FROM Nutrek WHERE food_name LIKE " + str("'%"+food+"%'") + ";")
+            results1 = cursor1.fetchall()
+            cursor2 = self.connection.cursor()
+            cursor2.execute("SELECT Phosphorus_P_mg, Potassium_K_mg, Protein_g, Riboflavin_mg, Selenium_Se_mcg, Sodium_Na_mg, Sugars_added_g, Sugars_total_g, Thiamin_mg, Total_lipid_fat_g, Total_sugar_alcohols_g, Vitamin_A_IU , Vitamin_B12_mcg, Vitamin_B6_mg, Vitamin_C_total_ascorbic_acid_mg, Vitamin_D_IU, Vitamin_E_label_entry_primarily_IU, Vitamin_K_phylloquinone_mcg, Water_g, Xylitol_g, Zinc_Zn_mg FROM Nutrek WHERE food_name LIKE " + str("'%"+food+"%'") + ";")
+            results2 = cursor2.fetchall()
+            fullNutrientList = []
+            results = []
+            for i in results1[0]:
+                results.append(i)
+            for j in results2[0]:
+                results.append(j)
+            nutrientDictionary = {}
+            for nutrient, proportion in zip(nutrientList, results):
+                nutrientDictionary[nutrient] = proportion
+            return nutrientDictionary
 
         except Exception as e:
             print ("Something went wrong when executing the query: ", e)
@@ -58,14 +63,23 @@ class Nutrek:
 
     def getIngredientBreakDown(self, food):
         ''' returns all the ingredients in a given food item'''
-        # ***contains method doesn't seem to be working - we want to see if the "food_name" OR branded food name contains the user input
-        # i.e. user types in milk and query finds the rows containing milk in the food name column.
-        food = str(food.upper())
+        food = food.upper()
         try:
-            cursor = self.connection.cursor ()
-            cursor.execute("SELECT food_name,ingredients_english FROM Nutrek WHERE food_name ~ (%s)",(food,))
+            cursor = self.connection.cursor()
+            query = ("SELECT ingredients_english FROM Nutrek WHERE  food_name LIKE " + str("'%"+food+"%'") +";")
+            cursor.execute(query)
             results = cursor.fetchall()
-            return str(results[0])
+            results = results[0]
+            FullIngredientList = []
+            for item in results:
+                if "(" in item:
+                    item = item.replace("(", "")
+                if "," in item:
+                    item = item.replace(",", "")
+                if ")" in item:
+                    item = item.replace(")","")
+                FullIngredientList.append(item)
+            return FullIngredientList
 
         except Exception as e:
             print ("Something went wrong when executing the query: ", e)
@@ -74,7 +88,7 @@ class Nutrek:
     def getFoodAvailable(self):
         '''returns all foods in database'''
         try:
-            cursor = self.connection.cursor ()
+            cursor = connection.cursor()
             query = "SELECT food_name FROM Nutrek"
             cursor.execute(query)
             results = cursor.fetchall()
@@ -84,45 +98,45 @@ class Nutrek:
             print ("Something went wrong when executing the query: ", e)
             return None
 
-
-
     def containsAllergen(self, food, allergen):
         '''returns True if food contains allergen (could cause allergic reaction) and false if otherwise '''
-        # ***how do we work with booleans in SQL? For example, we could find the food that contains key word of food enterered by user
-        # but then we need to see if the ingredients adjacent to that food contain an allergen and output true OR false accordingly
-        # if allergen in ingredients, then return True. Else (allergen not in ingredients list), return False.
-        ingredients = list(self.getIngredientBreakDown(food)) # how can we call the getIntredientBreakdown method to use here now if it
-        #is now using query
-        food = str(food.upper())
-        try:
-            cursor = self.connection.cursor ()
-            cursor.execute("SELECT food_name,ingredients_english FROM Nutrek WHERE (food_name ~ (%s)) AND (ingredients_english ~ (%s))",(food,allergen))
-            results = cursor.fetchall()
-            return str(results[0])
 
-        except Exception as e:
-            print ("Something went wrong when executing the query: ", e)
-            return None
-
-
-
-    def getNutrientThreshold(self, food, nutrient, nutritionTarget):
-        '''check if the amount of nutrients in a given food is meeting the indicated goal for a user'''
-        # ***Conditions we are trying to apply to SQL: 1. nutrition columns each need to be above the nutrition target otherwise, it will output false. also
-        # 2. the name of the food has to be valid and 3. the sum of the nutritents cannot be 0.
+        ingredients = self.getIngredientBreakDown(food)
+        FullIngredientList = []
+        allergen = allergen.upper()
+        for item in ingredients:
+            if "(" in item:
+                item = item.replace("(", "")
+            if "," in item:
+                item = item.replace(",", "")
+            if ")" in item:
+                item = item.replace(")","")
+            FullIngredientList.append(item)
         food = food.upper()
         try:
-            cursor = self.connection.cursor ()
-            query = "SELECT Ash_grams, Biotin_mcg, Caffeine_mg, Calcium_Ca_mg, Carbohydrate_by_difference_g, Carbohydrate_other_g, Cholesterol_mg, Chromium_Cr_mcg, Copper_Cu_mg, Fatty_acids_total_monounsaturated_g, Fatty_acids_total_polyunsaturated_g, Fatty_acids_total_saturated_g, Fatty_acids_total_trans_g, Fiber_insoluble_g, Fiber_soluble_g, Fiber_total_dietary_g, Folic_acid_mcg, Iodine_I_mcg, Iron_Fe_mg, Lactose_g, Magnesium_Mg_mg, Manganese_Mn_mg, Niacin_mg, Pantothenic_acid_mg, Phosphorus_P_mg, Potassium_K_mg, Protein_g, Riboflavin_mg, Selenium_Se_mcg, Sodium_Na_mg, Sugars_added_g, Sugars_total_g, Thiamin_mg, Total_lipid_fat_g, Total_sugar_alcohols_g, Vitamin_A_IU , Vitamin_B12_mcg, Vitamin_B6_mg, Vitamin_C_total_ascorbic_acid_mg, Vitamin_D_IU, Vitamin_E_label_entry_primarily_IU, Vitamin_K_phylloquinone_mcg, Water_g, Xylitol_g, Zinc_Zn_mg" + "FROM Nutrek" + "WHERE (food_name == " + str(food) + ") AND (Ash_grams  <> 0 OR Biotin_mcg  <> 0 OR Caffeine_mg  <> 0 OR Calcium_Ca_mg  <> 0 OR Carbohydrate_by_difference_g  <> 0 OR Carbohydrate_other_g  <> 0 OR Cholesterol_mg  <> 0 OR Chromium_Cr_mcg  <> 0 OR Copper_Cu_mg  <> 0 OR Fatty_acids_total_monounsaturated_g  <> 0 OR Fatty_acids_total_polyunsaturated_g  <> 0 OR Fatty_acids_total_saturated_g  <> 0 OR Fatty_acids_total_trans_g  <> 0 OR Fiber_insoluble_g  <> 0 OR Fiber_soluble_g  <> 0 OR Fiber_total_dietary_g  <> 0 OR Folic_acid_mcg  <> 0 OR Iodine_I_mcg  <> 0 OR Iron_Fe_mg  <> 0 OR Lactose_g  <> 0 OR Magnesium_Mg_mg  <> 0 OR Manganese_Mn_mg  <> 0 OR Niacin_mg  <> 0 OR Pantothenic_acid_mg  <> 0 OR Phosphorus_P_mg  <> 0 OR Potassium_K_mg  <> 0 OR Protein_g  <> 0 OR Riboflavin_mg  <> 0 OR Selenium_Se_mcg  <> 0 OR Sodium_Na_mg  <> 0 OR Sugars_added_g  <> 0 OR Sugars_total_g  <> 0 OR Thiamin_mg  <> 0 OR Total_lipid_fat_g  <> 0 OR Total_sugar_alcohols_g  <> 0 OR Vitamin_A_IU   <> 0 OR Vitamin_B12_mcg  <> 0 OR Vitamin_B6_mg  <> 0 OR Vitamin_C_total_ascorbic_acid_mg  <> 0 OR Vitamin_D_IU  <> 0 OR Vitamin_E_label_entry_primarily_IU  <> 0 OR Vitamin_K_phylloquinone_mcg  <> 0 OR Water_g  <> 0 OR Xylitol_g  <> 0 OR Zinc_Zn_mg  <> 0)"
-            cursor.execute(query)
-            results = cursor.fetchall()
-            return str(results[0])
-
+            for ing in FullIngredientList:
+                if allergen in ing:
+                    return True
+            return False
         except Exception as e:
             print ("Something went wrong when executing the query: ", e)
             return None
 
-        pass
+
+
+    def checkNutrientThreshold(self, food, nutrient):
+        '''check if the amount of nutrients in a given food is meeting the indicated goal for a user
+        we need to define this. Greater or Less than?'''
+        food = food.upper()
+        nutrient = nutrient.lower()
+        try:
+            nutrientDictionary = self.getNutrients(food)
+            for item in nutrientDictionary:
+                if nutrient in item:
+                    return item, nutrientDictionary[item]
+        except Exception as e:
+            print ("Something went wrong when executing the query: ", e)
+            return None
 
 def main():
     user = 'odoome'
@@ -132,11 +146,13 @@ def main():
     # Connect to the database
     N = Nutrek()
     N.connect(user, password)
-    # print(N.getAllColumns())
     print(N.getNutrients('granola'))
+    print("\n")
     print(N.getIngredientBreakDown('granola'))
-    print(N.containsAllergen('granola', 'peanuts'))
-
+    print("\n")
+    print(N.containsAllergen('granola', 'peanut'))
+    print("\n")
+    print(N.checkNutrientThreshold('granola', 'protein'))
 
     # Disconnect from database
     N.disconnect()
