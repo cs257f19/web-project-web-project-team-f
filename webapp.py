6bafa1b8f09be594705db1ceec19286563a2210f
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-11-16 05:19:22 -0600

    Add files via upload

---
+++ webapp.py
@@ -0,0 +1,21 @@
+import flask
+from flask import render_template
+import json
+import sys
+
+app = flask.Flask(__name__)
+
+@app.route('/web/')
+def boring():
+    return render_template('nutrek.html')
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Usage: {0} host port'.format(sys.argv[0]), file=sys.stderr)
+        exit()
+
+    host = sys.argv[1]
+    port = sys.argv[2]
+    app.run(host=host, port=port)
