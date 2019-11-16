e4e866bd501081b34fa8970d33e85b0508be13e2
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-11-16 05:57:31 -0600

    Delete createTable.sql

--- createTable.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS Serving;
-CREATE TABLE Serving (
-  NDB_No integer,
-  Serving_Size integer,
-  Serving_Size_UOM text,
-  Household_Serving_Size integer,
-  Household_Serving_Size_UOM text,
-);
