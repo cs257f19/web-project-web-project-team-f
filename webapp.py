66151b39a97d5abdda4eda6e94103948e7d35337
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-11-18 11:41:35 -0600

    Add files via upload

--- webapp.py
+++ webapp.py
@@ -4,8 +4,9 @@ import json
 import sys
 
 app = flask.Flask(__name__)
+app = flask.Flask(static_folder='static')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app = Flask(static_folder='static')
+
 
 @app.route('/')
 def nutrek():
