d6f557317f01c78a6a4dc7a3f02738093e2285da
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-11-20 14:23:35 -0600

    Add files via upload

--- testDataSource.py
+++ testDataSource.py
@@ -7,22 +7,22 @@ class DataSourceTester(unittest.TestCase):
     def setUp(self) -> None:
         self.nutrek = Nutrek()
 
-    def test_containsAllergy_bothValidInputs(self):
+    def test_containsAllergen_bothValidInputs(self):
         food1 = 'granola'
         allergen1 = 'peanut'
-        result1 = self.nutrek.containsAllergy(food1, allergen1)
+        result1 = self.nutrek.containsAllergen(food1, allergen1)
         self.assertTrue(result1)
 
-    def test_containAllergy_firstInValidInput(self):
+    def test_containAllergen_firstInValidInput(self):
         food2 = 'milk'
         allergen2 = 'Chairs'
-        result2 = self.nutrek.containsAllergy(food2, allergen2)
+        result2 = self.nutrek.containsAllergen(food2, allergen2)
         self.assertFalse(result2)
 
-    def test_containsAllergy_secondInvalidInput(self):
+    def test_containsAllergen_secondInvalidInput(self):
         food3 = 'grain'
         allergen3 = 'helicopter'
-        result3 = self.nutrek.containsAllergy(food3, allergen3)
+        result3 = self.nutrek.containsAllergen(food3, allergen3)
         self.assertFalse(result3)
 
 if __name__ == '__main__':
