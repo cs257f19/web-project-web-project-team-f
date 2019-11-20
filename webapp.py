ce1e0489d38294ec2049f61a1b60aec6d4ce9b63
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-11-20 14:02:31 -0600

    Add files via upload

--- webapp.py
+++ webapp.py
@@ -14,7 +14,7 @@ def getNutritionInfo():
         ds = datasource.Nutrek()
         description = "displaying all nutrients in food and their proportions"
         result = ds.getNutrients(food)
-    return render_template('nutrekPrototype.html')
+    return render_template('nutrekPrototype.html', result=result, description=description)
 
 
 if __name__ == '__main__':
