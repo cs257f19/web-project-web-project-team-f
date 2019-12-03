44fb64c39aa5f52742482ff8f5fb80f2a13b185d
aisheem <49172107+aisheem@users.noreply.github.com>
2019-12-02 20:40:46 -0800

    Update webapp.py
    Comment within ''' ''' may be alternative solution to getting data from form

--- webapp.py
+++ webapp.py
@@ -4,12 +4,15 @@ import json
 import sys
 import datasource
 
+# Connect to database
 ds = datasource.Nutrek()
 user = 'odoome'
 password = 'tiger672carpet'
 ds.connect(user, password)
+
 app = flask.Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 @app.route('/', methods = ["GET", "POST"])
 def home():
     return render_template('nutrekPrototype.html')
@@ -25,12 +28,21 @@ def getResults():
 #         result = request.form
 #         food = result['food']
 #         ds = datasource.Nutrek()
-#         description = "displaying all nutrients in food and their proportions"
+#         description = "Displaying nutrient breakdown of food"
 # #         result = ds.getNutrients(food)
 # #         return render_template('results.html')
 #         return description  
+'''Translating HTML form data into a database query and then into a results page
 
-
+@app.route('/resultnutrients', methods = ['POST', 'GET'])
+def resultNutrients():
+    if request.method == 'POST':
+        result = request.form
+        ds = datasource.Nutrek()
+        description = "Displaying nutrient breakdown for" + result.get("food")
+        result = ds.getNutrients(result.get("food"))
+        return render_template('result.html', result = result, description = description)'''
+    
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print('Usage: {0} host port'.format(sys.argv[0]), file=sys.stderr)
