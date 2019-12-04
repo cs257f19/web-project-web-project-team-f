80d4e292c369b6db122d677af7eb9fa274230006
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-12-04 15:12:11 -0500

    Update testDataSource.py

--- testDataSource.py
+++ testDataSource.py
@@ -21,13 +21,20 @@ class DataSourceTester(unittest.TestCase):
         food2 = 'milk'
         allergen2 = 'Chairs'
         result2 = self.nutrek.containsAllergen(food2, allergen2)
-        return result2 
+        self.assertFalse(result2) 
 
     def test_containsAllergen_secondInvalidInput(self):
         food3 = 'grain'
         allergen3 = 'helicopter'
         result3 = self.nutrek.containsAllergen(food3, allergen3)
-        return result3 
+        self.assertFalse(result3)
+    
+    def test_containsAllergen_emptyInput1(self):
+        food4 = ""
+        allergen4 = "Lactose"
+        result4 = self.nutrek.containsAllergen(food4, allergen4)
+       
+        
 
 if __name__ == '__main__':
     unittest.main()
