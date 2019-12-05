554033b8c8d3940e49d1df2e486f16070747675c
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-12-05 02:42:56 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -31,10 +31,7 @@ def resultNutrients():
             result = ds.getNutrients(food)
             if result is None:
                 return "This item "+ food + " does not exist in our database."
-            allResults = ""
-            for i in result:
-                allResults += i
-            return render_template("results.html", result = allResults)
+            return render_template("results.html", result = result)
         elif querySelection == "ingredients":
             ingredients = ds.getIngredientBreakDown(food)
             if ingredients is None:
