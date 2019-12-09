f1586b663cee827d08401ef9620c776746c19a37
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-12-09 01:54:01 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -23,13 +23,16 @@ def home():
 def aboutData():
     return render_template("Data.html")
 
-# def getProductName(food):
-#     allFood = ds.getFoodAvailable(food)
-#     productName = allFood[0]
-#     result = ""
-#     for item in productName:
-#        result += item + " "
-#     return result 
+def getProductName(food):
+    nutrients = ds.getNutrients(food)
+    food = list(nutrients.keys()[0]
+    allFood = ds.getFoodAvailable(food)
+    searchIndex = nutrients[food]
+    productName = allFood[searchIndex]
+    result = ""
+    for item in productName:
+       result += item + " "
+    return result 
 	
 '''Translates HTML form data into a database query and then into a results page'''
 @app.route("/results", methods = ["POST", "GET"])
@@ -65,6 +68,8 @@ def getResults():
                 return render_template("ingredients.html", result=result)
             allIngredients = {}
 #             allIngredients[(food)]=0
+	    foodName = getProductName(food)
+	    allIngredients[foodName] = 0 
             for item,index in enumerate(ingredients):
                 allIngredients[index] = item
             return render_template("ingredients.html", result=allIngredients)
@@ -82,11 +87,11 @@ def getResults():
                 result = ds.containsAllergen(food, allergen)
                 
                 if result is True:
-                   result =  "WARNING! " + food + " contains the allergen: " + allergen
+                   result =  "WARNING! " + getProductName(food) + " contains the allergen: " + allergen
                 elif result is False:
-                    result =  "No known " + allergen + " allergen in " + food + " according to USDA Food database."
+                    result =  "No known " + allergen + " allergen in " + getProductName(food) + " according to USDA Food database."
                 else:
-                    result =  "No known " + allergen + " allergen in " + food + " according to USDA Food database."
+                    result =  "No known " + allergen + " allergen in " + getPorductName(food) + " according to USDA Food database."
                 result = {result:0}
             return render_template("allergens.html", result=result)
 
