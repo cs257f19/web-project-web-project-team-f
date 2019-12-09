ff451fd8b0e87878a38150571c344e43c8a81ec2
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-12-09 02:38:07 -0500

    Update datasource.py

--- datasource.py
+++ datasource.py
@@ -87,7 +87,7 @@ class Nutrek:
                 for item in allFood[rehashed]:
                     finalFood += item + " "
                
-                nutrientDictionary[finalFood] = rehashed
+                nutrientDictionary[finalFood] = 0
                 proportions = list(nutrientDictionary.values())
                 proportionsList = []
                 for item in proportions:
