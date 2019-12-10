0ff0f8e9632b1387315f119fcbfbf9471bb2761d
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-12-09 22:15:58 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -47,7 +47,9 @@ def getSearchResults():
             return render_template("searchResults.html", result=result)
         allProducts = {}
         for item,index in enumerate(searchresults):
-            finalProduct = "".join(index)
+            finalProduct = " ".join(index)
+            productList = finalProduct.split(" ")
+            finalProduct = " ".join(productList)
             allProducts[finalProduct] = item
         return render_template("searchResults.html", result=allProducts)
 
