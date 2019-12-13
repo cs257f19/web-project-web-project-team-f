5e2d9af7c872cef32ecd7d8acd4db4c14b85dc06
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-12-13 00:29:22 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -81,12 +81,25 @@ def getResults():
             allIngredients = {}
             allIngredients[food]=0
             ingredients = ingredients[0]
-#             if len(ingredients) == 1:
-#                 allIngredients[ingredients] = 1
-#                 return render_template("ingredients.html", result=allIngredients)
+            if len(ingredients) == 1:
+                allIngredients[ingredients] = 1
+                return render_template("ingredients.html", result=allIngredients)
             ingredients = ingredients.split(",")
+            opening = "({["
+            closing = ")}]"
+            newNutrient = ""
             for item,index in enumerate(ingredients):
-                allIngredients[index] = item
+                for char in index:
+                    if char in opening:
+                        newNutrient += index + " "
+                        for j in ingredients[item:]:
+                            for char in j:
+                                if char in closing:
+                                    allIngredients[newNutrient] = item 
+                                else:
+                                    newNutrient += j
+                    else:
+                        allIngredients[index] = item
             return render_template("ingredients.html", result=allIngredients)
         
         elif querySelection == "allergy":
