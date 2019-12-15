a0b893610c7f046c84f911f65bbe0b8a7f144b9b
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-12-15 02:42:27 -0500

    Update datasource.py

--- datasource.py
+++ datasource.py
@@ -188,7 +188,7 @@ def main():
     # Connect to the database
     N = Nutrek()
     N.connect(user, password)
-#     print(N.containsAllergen("granola", "peanuts"))
+    print(N.containsAllergen("100% GINGER IMMUNITY SHOT", "peanuts"))
 #     print(N.containsAllergen("milk", "lactose"))
 #     print(N.containsAllergen("fried rice", "oil"))
 #     Disconnect from database
