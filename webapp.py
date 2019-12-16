db766fccec2ca0b856260b6c0868a037c9b9295f
aisheem <49172107+aisheem@users.noreply.github.com>
2019-12-16 02:29:49 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -32,8 +32,8 @@ def getSearchResults():
             result = {result:result}
             return render_template("searchResults.html", result=result)
         searchresults = ds.getFoodAvailable(foodsearched)
+        print(searchresults)
         if  searchresults is None:
-            print("hello")
             noresults =  "No results for " + foodsearched + ". Search new food."
             noresults = {noresults:noresults}
             return render_template("searchResults.html", result=noresults)
