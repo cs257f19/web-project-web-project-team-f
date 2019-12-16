fab413fec63063791316120ba5a454d5eb2463de
Eric Odoom <40000187+odoome101@users.noreply.github.com>
2019-12-15 22:09:29 -0500

    Update datasource.py

--- datasource.py
+++ datasource.py
@@ -203,8 +203,8 @@ def main():
     N.connect(user, password)
 #     print(N.containsAllergen("10 OZ UNIFORM SALMON PORT", "salmon"))
 #     print(N.containsAllergen("100% GINGER IMMUNITY SHOT", "peanuts"))
-      print(N.containsAllergen("granola", "nut"))
-      print(N.getIngredientBreakDown("granola"))
+    print(N.containsAllergen("granola", "nut"))
+    print(N.getIngredientBreakDown("granola"))
 #     print(N.containsAllergen("fried rice", "oil"))
 #     Disconnect from database
 #     N.disconnect()
