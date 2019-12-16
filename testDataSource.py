dba232607511767ef5dfc089b95da740a0725b55
aisheem <49172107+aisheem@users.noreply.github.com>
2019-12-15 19:18:55 -0800

    Add files via upload

--- testDataSource.py
+++ testDataSource.py
@@ -9,47 +9,47 @@ class DataSourceTester(unittest.TestCase):
         password = "tiger672carpet"
         self.nutrek.connect(user, password)
 
+    '''allergen is in valid food'''
     def testContainsAllergenBothValidInputs(self):
         food1 = "granola"
-        allergen1 = "nut"
+        allergen1 = "peanut"
         result1 = self.nutrek.containsAllergen(food1, allergen1)
         self.assertTrue(result1)
-
+    
+    '''allergen is not in valid food'''
     def testContainsAllergenFirstInvalidInput(self):
-        food2 = "tuna"
-        allergen2 = "Milk"
+        food2 = "grain"
+        allergen2 = "helicopters"
         result2 = self.nutrek.containsAllergen(food2, allergen2)
         self.assertFalse(result2) 
 
+    '''food name is not valid'''
     def testContainsAllergenSecondInvalidInput(self):
-        food3 = "grain"
-        allergen3 = "helicopter"
+        food3 = "aishee"
+        allergen3 = "peanut"
         result3 = self.nutrek.containsAllergen(food3, allergen3)
-        self.assertFalse(result3)
+        self.assertIsNone(result3)
+   
+    '''food is empty'''
+    def testContainsAllergenFirstEmptyInput(self):
+        food4 = ""
+        allergen4 = "Lactose"
+        result4 = self.nutrek.containsAllergen(food4, allergen4)
+        self.assertIsNone(result4)
     
-#     def testContainsAllergenBothInvalidInputs(self):
-#         food4 = "aishee"
-#         allergen4 = "helicopter"
-#         result4 = self.nutrek.containsAllergen(food4, allergen4)
-#         self.assertFalse(result4)
-
-    def testcontainsAllergenFirstEmptyInput(self):
-        food5 = ""
-        allergen5 = "Lactose"
+    '''allergen is empty'''
+    def testContainsAllergenSecondEmptyInput(self):
+        food5 = "chicken"
+        allergen5 = ""
         result5 = self.nutrek.containsAllergen(food5, allergen5)
         self.assertIsNone(result5)
     
-    def testcontainsAllergenSecondEmptyInput(self):
-        food6 = "chicken"
+    '''food and allergen empty'''
+    def testContainsAllergenBothEmptyInputs(self):
+        food6 = ""
         allergen6 = ""
         result6 = self.nutrek.containsAllergen(food6, allergen6)
         self.assertIsNone(result6)
-    
-    def testcontainsAllergenBothEmptyInputs(self):
-        food7 = ""
-        allergen7 = ""
-        result7 = self.nutrek.containsAllergen(food7, allergen7)
-        self.assertIsNone(result7)
                    
 if __name__ == "__main__":
     unittest.main()
