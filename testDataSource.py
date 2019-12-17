274403281dc9e8061422cf0de64fd48dc8573e39
aisheem <49172107+aisheem@users.noreply.github.com>
2019-12-16 20:13:08 -0800

    Update testDataSource.py w/ correct comment style.

--- testDataSource.py
+++ testDataSource.py
@@ -9,43 +9,43 @@ class DataSourceTester(unittest.TestCase):
         password = "tiger672carpet"
         self.nutrek.connect(user, password)
 
-    '''allergen is in valid food name'''
     def testContainsAllergenBothValidInputs(self):
+        '''allergen is in valid food name'''
         food1 = "granola"
         allergen1 = "nut"
         result1 = self.nutrek.containsAllergen(food1, allergen1)
         self.assertTrue(result1)
     
-    '''allergen is not in valid food name'''
     def testContainsAllergenFirstInvalidInput(self):
+        '''allergen is not in valid food name'''
         food2 = "grain"
         allergen2 = "helicopters"
         result2 = self.nutrek.containsAllergen(food2, allergen2)
         self.assertFalse(result2) 
 
-    '''food name is not valid (not empty)'''
     def testContainsAllergenSecondInvalidInput(self):
+        '''food name is not valid (not empty)'''
         food3 = "aishee"
         allergen3 = "peanut"
         result3 = self.nutrek.containsAllergen(food3, allergen3)
         self.assertIsNone(result3)
    
-    '''food name is empty'''
     def testContainsAllergenFirstEmptyInput(self):
+        '''food name is empty'''
         food4 = ""
         allergen4 = "Lactose"
         result4 = self.nutrek.containsAllergen(food4, allergen4)
         self.assertIsNone(result4)
     
-    '''allergen is empty'''
     def testContainsAllergenSecondEmptyInput(self):
+        '''allergen is empty'''
         food5 = "chicken"
         allergen5 = ""
         result5 = self.nutrek.containsAllergen(food5, allergen5)
         self.assertIsNone(result5)
     
-    '''food and allergen both empty'''
     def testContainsAllergenBothEmptyInputs(self):
+        '''food and allergen both empty'''
         food6 = ""
         allergen6 = ""
         result6 = self.nutrek.containsAllergen(food6, allergen6)
