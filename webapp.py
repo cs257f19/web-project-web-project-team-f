dbf091f898f40dbc7761e3a2b33357991ff36223
aisheem <49172107+aisheem@users.noreply.github.com>
2019-12-16 22:57:50 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -48,7 +48,6 @@ def getSearchResults():
                 removedDuplicates[key] = allProducts[key] 
         if len(removedDuplicates)==1: # if food searched was exact match to a single food
             removedDuplicates["foodsearched"] = foodsearched
-        print(len(removedDuplicates))
         return render_template("searchResults.html", result=removedDuplicates)
 
 def nutritionResults(food):
